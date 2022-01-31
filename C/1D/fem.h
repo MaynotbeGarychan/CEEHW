34c12252d02999a9784ec5d5b959f8617c1361bd
Jiawei CHEN <garychan0124@gmail.com>
2022-01-31 08:43:36 +0800

    delete some files

--- C/1D/fem.h
+++
@@ -1,2 +0,0 @@
-void main_allocate(int,int,int);
-void cal_amat(int n,int ne,int kd,const int* cny,const int* num,const float* coor,const float* younglst,const float* u,float* r);
--- C/report4-1/header.h
+++
@@ -1,2 +0,0 @@
-void main_allocate(int,int);
-void func(int n,int ne,const int* cny,const float* mat,const float* u,float* r);
